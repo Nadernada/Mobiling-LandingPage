31aa098bca03fd38c46f10054e06868fb1fa2c3a
Nadernada <59936807+Nadernada@users.noreply.github.com>
2023-08-16 02:47:44 +0100

    updated paths for the assets

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className='flex flex-col lg:flex-row w-full justify-between pt-[36px] lg:pt-[72px]'>
         <div className='flex flex-col lg:w-1/3 items-start'>
           <div className='flex flex-row justify-center items-center'>
-            <img src='/logo-b&w.svg' className='w-[30px] h-[34px] object-contain mr-[12px]' />
+            <img src='../assets/logo-b&w.svg' className='w-[30px] h-[34px] object-contain mr-[12px]' />
             <div className='flex flex-col justify-start'>
               <h5 className='text-[20px] font-medium text-light leading-[25px] mb-[5px]'>Mobiling</h5>
               <p className='text-[12px] font-light text-light leading-[15px]'>Automobile</p>
@@ -16,10 +16,10 @@ const Footer = () => {
           </div>
           <p className='w-[80%] text-[16px] font-normal text-light leading-[20px] mt-[26px]' >As an upscale and exclusive chauffeur and limousine service near Indonesia, we have been ensuring for more than 15 years that you get to your destination on time and safely.</p>
           <div className='flex flex-row justify-center items-center gap-[24px] mt-[40px]'>
-            <img src='/twitter.svg' alt='facebook' className={`${styles.iconsSm}`} />
-            <img src='/facebook.svg' alt='facebook' className={`${styles.iconsSm}`} />
-            <img src='/linkedin.svg' alt='linkedin' className={`${styles.iconsSm}`} />
-            <img src='/instagram.svg' alt='instagram' className={`${styles.iconsSm}`} />
+            <img src='../assets/twitter.svg' alt='facebook' className={`${styles.iconsSm}`} />
+            <img src='../assets/facebook.svg' alt='facebook' className={`${styles.iconsSm}`} />
+            <img src='../assets/linkedin.svg' alt='linkedin' className={`${styles.iconsSm}`} />
+            <img src='../assets/instagram.svg' alt='instagram' className={`${styles.iconsSm}`} />
           </div>
         </div>
         <FooterLinks />
@@ -32,9 +32,9 @@ const Footer = () => {
           <li className={`${styles.footerLinks} lg:leading-[36px]`}>Cookies</li>
         </ul>
         <button className='bg-transparent text-light flex flex-row items-center justify-center mt-[30px] lg:mt-0'><span>
-          <img src='/globe.svg' alt='globe' className={`${styles.iconsSm} pr-[16px]`} /></span>
+          <img src='../assets/globe.svg' alt='globe' className={`${styles.iconsSm} pr-[16px]`} /></span>
           English<span>
-          <img src='/arrow-right.svg' alt='arrow-right' className={`${styles.iconsSm} pl-[16px]`} /></span>
+          <img src='../assets/arrow-right.svg' alt='arrow-right' className={`${styles.iconsSm} pl-[16px]`} /></span>
         </button>
       </div>
     </footer>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <nav className='w-full h-[120px] py-[36px] lg:px-[80px] xl:px-[140px] px-[50px] flex justify-between items-center '>
       <div className='flex flex-row justify-center items-center'>
-        <img src='.../public/logo-colored.svg' className='w-[30px] h-[34px] object-contain mr-[12px]' />
+        <img src='../assets/logo-colored.svg' className='w-[30px] h-[34px] object-contain mr-[12px]' />
         <div className='flex flex-col justify-start'>
           <h3 className='text-[20px] font-medium text-blue-600 leading-[25px] mb-[5px]'>Mobiling</h3>
           <p className='text-[12px] font-light text-mid leading-[15px]'>Automobile</p>
@@ -24,7 +24,7 @@ const Navbar = () => {
         </div>
         <button className='bg-blue-600 hover:bg-blue-800 text-[18px] font-normal text-light leading-[22.5px] py-[12px] px-[40px] mt-[12px] lg:mt-0 rounded-[10px] transition-all z-10'>Sign in</button>
       </div>
-      <img src={`${isOpen ? '/close.svg' : '/burger-menu.svg'}`} alt='menu' className={`block lg:hidden w-[32px] h-[32px] object-contain z-30 cursor-pointer transition-all`} onClick={()=>setIsOpen(!isOpen)}/>
+      <img src={`${isOpen ? '../assets/close.svg' : '../assets/burger-menu.svg'}`} alt='menu' className={`block lg:hidden w-[32px] h-[32px] object-contain z-30 cursor-pointer transition-all`} onClick={()=>setIsOpen(!isOpen)}/>
     </nav>
   )
 }
