61eaaa1a63c4c95fcd138ed819b4a3eb2af81258
Nadernada <59936807+Nadernada@users.noreply.github.com>
2023-08-16 03:30:14 +0100

    Fixing the paths

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <div className='flex lg:flex-row flex-col items-center mt-[25px] z-10' dir='ltr'>
           <input type='text' name='query' placeholder='Search' className='lg:w-[450px] w-full bg-white px-[32px] focus-visible:outline-blue-600 py-[12px] rounded-t-[10px] lg:rounded-l-[10px] lg:rounded-tr-none xs:max-lg:rounded-t-[10px] text-[18px] leading-[22.5px]' />
           <div className='w-full lg:w-[47px] flex flex-row justify-center items-center lg:justify-center bg-blue-600 p-[15px] rounded-b-[10px] lg:rounded-bl-none lg:rounded-r-[10px]  cursor-pointer'>
-            <img src='./src/assets/search.svg' alt='search' className='w-[17px] h-[17px] object-contain' />
+            <img src='./assets/search.svg' alt='search' className='w-[17px] h-[17px] object-contain' />
             <p className='text-[18px] font-medium text-light block lg:hidden ml-[8px]'>search</p>
           </div>
         </div>
@@ -36,13 +36,13 @@ const Hero = () => {
       <div className='xl:basis-1/3 basis-4/12 h-full flex flex-col items-center justify-between static mt-0 lg:mt-[120px] md:gap-[45px] lg:gap-0'>
         <div className='hidden lg:block w-[300px] h-full bg-blue-500 absolute top-0 z-0' />
         <div className='w-[90px] h-[90px] flex items-center'>
-          <img src='./src/assets/stamp.svg' alt='stamp' className='w-[90px] h-[90px] object-contain animate-spin-slow origin-center' />
-          <img src='./src/assets/star.svg' alt='star' className='w-[28px] h-[28px] relative -left-[59px] animate-spin-slow origin-center' />
+          <img src='./assets/stamp.svg' alt='stamp' className='w-[90px] h-[90px] object-contain animate-spin-slow origin-center' />
+          <img src='./assets/star.svg' alt='star' className='w-[28px] h-[28px] relative -left-[59px] animate-spin-slow origin-center' />
         </div>
-        <img src='./src/assets/Hero.png' alt='Car' className='xl:w-[600px] lg:w-[400px] h-auto aspect-auto  lg:absolute lg:-bottom-[110px] xl:-bottom-[180px] 2xl:-bottom-[110px] z-10' />
+        <img src='./assets/Hero.png' alt='Car' className='xl:w-[600px] lg:w-[400px] h-auto aspect-auto  lg:absolute lg:-bottom-[110px] xl:-bottom-[180px] 2xl:-bottom-[110px] z-10' />
         <div className='flex flex-row w-full lg:w-[60%] items-center justify-around relative lg:top-[400px] mt-[16px] lg:mt-0'>
-          <img src='./src/assets/chevron-left.svg' alt='chevron-left' className='w-[32px] h-[32px] object-contain cursor-pointer' />
-          <img src='./src/assets/chevron-right.svg' alt='chevron-right' className='w-[32px] h-[32px] object-contain cursor-pointer' />
+          <img src='./assets/chevron-left.svg' alt='chevron-left' className='w-[32px] h-[32px] object-contain cursor-pointer' />
+          <img src='./assets/chevron-right.svg' alt='chevron-right' className='w-[32px] h-[32px] object-contain cursor-pointer' />
         </div>
       </div>
     </section>
